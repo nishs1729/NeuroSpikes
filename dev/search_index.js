f6d8b8b61170013b11555eb77a66e40103dbab35
Documenter.jl <documenter@juliadocs.github.io>
2023-12-22 09:34:02 +0000

    build based on df295aa

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NeuroSpikes","category":"page"},{"location":"#NeuroSpikes","page":"Home","title":"NeuroSpikes","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NeuroSpikes.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NeuroSpikes]","category":"page"}]
+}
