5f313711e177403022b7033bfd39c12cc9067eed
nish <nishs1729@gmail.com>
2024-01-03 16:53:55 +0530

    added an implementation of SPIKE_distance()

--- src/measures.jl
+++ src/measures.jl
@@ -1,19 +1,27 @@
 function SPIKE_distance(spike_train, tstart=0, tstep=0.1, tend=5)
-    tspan = tstart:tstep:tend
-    for st in spike_train
+    for st in spike_train # Sanity check
         tstep ≤ minimum(st[2:end] - st[1:end-1]) || error("Timestep should be smaller than the minimum ISI")
         pushfirst!(st, tspan[1])
         push!(st,tspan[end])
     end
 
-    tc = 2
-    tlow = spike_train[1]
+    tc = [2,2]
+    tPF = [[0.0], [0.0]]
+    ISI = [0.0, 0.0]
     for t in tspan
-        if t ≤ spike_train[1][tc]
-            println(t, spike_train[1][tc-1:tc])
-        else
-            tc += 1
-            println(t, spike_train[1][tc-1:tc])
+        for i in 1:2    # Get tPF and ISI at time t for both spike trains
+            tPF[i] = spike_train[i][tc[i]-1:tc[i]]
+            ISI[i] = spike_train[i][tc[i]] - spike_train[i][tc[i]-1]
+            if t > spike_train[i][tc[i]]
+                tc[i] += 1 
+                tPF[i] = spike_train[i][tc[i]-1:tc[i]]
+                ISI[i] = spike_train[i][tc[i]] - spike_train[i][tc[i]-1]
+            end
         end
+        # println(t, ' ', tc, ' ', ISI, ' ', tPF)
     end
 end
+
+spike_train = [sort(rand(Float64, 5)*tend), sort(rand(Float64, 5)*tend)]
+
+SPIKE_distance(spike_train)
